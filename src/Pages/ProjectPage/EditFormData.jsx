7673b66fe18e875368518e41aa652d82156aa702
Bayshakhe Pramanik <bayshakhepramanik@gmail.com>
2023-09-18 21:19:28 +0600

    successfully implemented PUT method

---
+++ src/Pages/ProjectPage/EditFormData.jsx
@@ -0,0 +1,108 @@
+import {
+  Box,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from "@mui/material";
+import { useState } from "react";
+import PersonalInfo from "./PersonalInfo";
+import AddressInfo from "./AddressInfo";
+import Education from "./Education";
+import Cgpa from "./Cgpa";
+import Employment from "./Employment";
+import Refference from "./Refference";
+import { useForm } from "react-hook-form";
+import schema from "./YupValidation";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+const EditFormData = ({ data, setData }) => {
+  const methods = useForm({
+    resolver: yupResolver(schema),
+    mode: "onChage",
+    defaultValues: data,
+  });
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleForm = (data) => {
+    const cgpa = structuredClone(data.cgpa);
+    // console.log({ cgpa });
+    data.cgpa = cgpa.reduce((acc, curr) => {
+      acc[curr.class_name] = curr.point;
+      return acc;
+    }, {});
+    fetch("http://localhost:3200/info/1", {
+      method: "put",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        // setData(data);
+        console.log(data);
+      });
+    // setData(data);
+  };
+
+  return (
+    <div>
+      <Box component="form" onSubmit={methods?.handleSubmit(handleForm)}>
+        <Button variant="contained" color="warning" onClick={handleClickOpen}>
+          Edit
+        </Button>
+        <Dialog
+          open={open}
+          onClose={handleClose}
+          fullWidth
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">Update you Form</DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              {/* personal information */}
+              <PersonalInfo />
+
+              {/* address information */}
+              <AddressInfo />
+
+              {/* education */}
+              <Education />
+
+              {/* CGPA */}
+              <Cgpa />
+
+              {/* Employment History */}
+              <Employment />
+
+              {/* Refference */}
+              <Refference />
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button color="error" variant="contained" onClick={handleClose}>
+              Disagree
+            </Button>
+            <Button type="submit" color="primary" variant="contained" autoFocus>
+              Update
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </Box>
+    </div>
+  );
+};
+
+export default EditFormData;
--- src/Pages/ProjectPage/ProjectPage.jsx
+++ src/Pages/ProjectPage/ProjectPage.jsx
@@ -8,33 +8,118 @@ import Employment from "./Employment";
 import Refference from "./Refference";
 import Cgpa from "./Cgpa";
 import schema from "./YupValidation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import EditFormData from "./EditFormData";
 
 const ProjectPage = () => {
-  const [data, setData] = useState({})
+  const [data, setData] = useState({});
 
-// const setValues = {
-//   personalInformation: {
-//     firstName: data?.personalInformation?.firstName
-//   }
-// }
-  const methods = useForm({ resolver: yupResolver(schema), mode: "onChage", defaultValues:{
+  // useEffect(() => {
+  //   fetch("http://localhost:3200/info")
+  //     .then((res) => res.json())
+  //     .then((data) => setData(data[0]));
+  // }, [data]);
+
+  const defaultValues = {
     personalInformation: {
-      firstName: "hello"
-    }
-  } });
+      firstName: "",
+      lastName: "",
+      email: "",
+      dateOfBirth: "",
+      gender: "",
+      phone: "",
+    },
+    interests: "",
+    employmentHistory: "",
+    education: "",
+    cgpa: "",
+    additionalInfo: "",
+  };
+  const methods = useForm({
+    resolver: yupResolver(schema),
+    // mode: "onChage",
+    defaultValues,
+  });
 
+  const updateData = (data) => {
+    fetch("http://localhost:3200/info/1", {
+      method: "put",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+        console.log(data);
+      });
+  };
 
   const handleForm = (data) => {
     const cgpa = structuredClone(data.cgpa);
-    // console.log({ cgpa });
-    data.cgpa = cgpa.reduce((acc, curr) => {
+    console.log({ cgpa });
+    data.cgpa = cgpa?.reduce((acc, curr) => {
       acc[curr.class_name] = curr.point;
       return acc;
     }, {});
-    setData(data);
+    updateData(data);
   };
-  console.log(JSON.stringify(data))
+  // console.log(data);
+
+  useEffect(() => {
+    if (data) {
+      methods.reset({
+        personalInformation: {
+          firstName: data?.personalInformation?.firstName,
+          lastName: data?.personalInformation?.lastName,
+          email: data?.personalInformation?.email,
+          // dateOfBirth: data?.personalInformation?.dateOfBirth,
+          gender: data?.personalInformation?.gender,
+          phone: data?.personalInformation?.phone,
+        },
+        // employmentHistory: data?.employmentHistory,
+        // education: data?.education,
+        // cgpa: data?.cgpa,
+        // additionalInfo: data?.additionalInfo,
+        addressInformation:{
+          street: data?.addressInformation?.street,
+          city: data?.addressInformation?.city,
+          state: data?.addressInformation?.state,
+          zipCode: data?.addressInformation?.zipCode,
+      },
+      // education information
+      // education: [
+      //   {
+      //     degree: data?.education?.street,
+      //     graduationYear: data?.education?.graduationYear,
+      //     institution: data?.education?.institution,
+      // }
+      // ],
+      // cgpa
+      // cgpa: {
+      //         class_name: ,
+      //         point: ,
+      //     },
+      // employment history
+      // employmentHistory: yup.array().of(
+      //     yup.object({
+      //         company: ,
+      //         startDate: ,
+      //         endDate: ,
+      //         position: ,
+      //     })
+      // ),
+      // interests
+      // interests: data?.interests,
+      // additonal info
+      // additionalInfo: yup.object({
+      //     referredBy: yup.string(),
+      //     comments: yup.string(),
+      // }),
+      });
+    }
+  }, [data, methods]);
 
   return (
     <FormProvider {...methods}>
@@ -57,13 +142,15 @@ const ProjectPage = () => {
         {/* Refference */}
         <Refference />
 
-        <Box>
-        <Button type="submit" variant="contained" sx={{ marginTop: "20px", marginRight: "5px" }}>
-          Submit
-        </Button>
-        <Button color="warning" variant="contained" sx={{ marginTop: "20px", marginLeft: "5px" }}>
-          Edit
-        </Button>
+        <Box display="flex" justifyContent="center" alignItems="baseline">
+          <Button
+            type="submit"
+            variant="contained"
+            sx={{ marginTop: "20px", marginRight: "5px" }}
+          >
+            Submit
+          </Button>
+          <EditFormData data={data} setData={setData}></EditFormData>
         </Box>
       </Box>
     </FormProvider>
